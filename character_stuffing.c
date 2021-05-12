54aeca86847b88537e6b59c25e664bdc2e7bb182
Sai Charan Vikranth Paluri <64738319+Vikranth19@users.noreply.github.com>
2021-05-13 01:02:20 +0530

    Update character_stuffing.c

--- character_stuffing.c
+++ character_stuffing.c
@@ -1,71 +1,48 @@
 #include<stdio.h>
+#include<ctype.h>
 #include<string.h>
-#define DLE 16
-#define STX 2
-#define ETX 3
 
-int  main()
+#define max 100
 
-{
-	char ch;
-	char arr[100]={DLE,STX};
-	int i=2,j;
-	printf("\n Enter the data stream(CTRL+B->STX,CTRL+C->ETX,CTRL+P->DLE):\n");
-	
-	do
-	{
-		scanf("%c", &ch);
-		printf("char is",ch);
-		if(ch=='\n')
-			break;
-
-		if(ch==DLE)
-				{
-					arr[i++]=DLE;
-					printf("DLE");
-				}
-				else if(ch==2)
-					printf("STX");
-				else if(ch==3)
-					printf("ETX");
-				else
-					printf("%c",ch);
-				arr[i++]=ch;
-	}while(ch!='\n');
-	
-	arr[i++]=DLE;
-	arr[i++]=ETX;
-	
-	printf("\n The stuffed stream is \n");
-	
-	
-
-	for(j=0;j<i;j++)
-	{
-			if(arr[j]==DLE)
-				printf("DLE");
-			else if(arr[j]==STX)
-				printf("STX");
-			else if(arr[j]==ETX)
-				printf("ETX");
-			else
-				printf("%c",arr[j]);
-		}
-	
-	printf("\n The de-stuffed data is \n");
-	for(j=2;j<i-2;j++)
-	{
-		if(arr[j]==DLE)
-			{
-				printf("DLE");
-				j++;
-			}
-		else if(arr[j]==STX)
-			printf("STX");
-		else if(arr[j]==ETX)
-			printf("ETX");
-		else
-			printf("%c",arr[j]);
+int  main(){
+    int si=0,di=0;
+	char start[] = "DLE STX ";
+	char end[] = " DLE ETX";
+	char src[max]="";
+	char dest[max]="";
+	char destuffed_data[max]="";
+	printf("Enter the data: \n");
+	scanf("%s",src);
+	strcat(dest,start);
+	di = strlen(start);
+	while(src[si]!='\0'){
+	    if(((src[si]=='D') || (src[si]=='d')) && ((src[si+1]=='L') || (src[si+1]=='l')) && ((src[si+2]=='E') || (src[si+2]=='e'))){
+	        dest[di]='D';dest[di+1]='L';dest[di+2]='E';
+	        dest[di+3]='D';dest[di+4]='L';dest[di+5]='E';
+	        di+=6;
+	        si+=3;
+	    }
+	    else{
+	        dest[di++]=src[si++];
+	    }
 	}
-	return 0;
+	dest[di]='\0';
+	strcat(dest,end);
+	printf("the stuffed character byte is: %s\n",dest);
+	
+    // 	destuffing
+    int len = strlen(dest);
+    int k=0;
+    for(int j=8;j<strlen(src)+8;){
+        if((dest[j]=='D') && (dest[j+2]=='L') && (dest[j+3]=='E')){
+            destuffed_data[k]='D';destuffed_data[k+1]='D';destuffed_data[k+2]='D';
+            k+=3;
+            j+=6;
+        }
+        else{
+            destuffed_data[k++]=dest[j++];
+        }
+    }
+    destuffed_data[k]='\0';
+    printf("the destuffed character byte is: %s\n",destuffed_data);
 }
